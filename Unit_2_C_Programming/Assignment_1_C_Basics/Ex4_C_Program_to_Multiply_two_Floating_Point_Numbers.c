2982005fd40b3b42a5553ce4c2a3fdeb0bf15a67
Kareem-Walid <engkareemwalid@gmail.com>
2023-03-26 23:46:16 -0700

    Uploade ex4 and ex5

---
+++ Unit_2_C_Programming/Assignment_1_C_Basics/Ex4_C_Program_to_Multiply_two_Floating_Point_Numbers.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 26, 2023
+ *      Author: Kareem Walid
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	float num1,num2,sum;
+	printf("Enter two numbers: ");
+	scanf("%f%f",&num1,&num2);
+	sum = num1 * num2;
+	printf("%f",sum);
+
+}
+
